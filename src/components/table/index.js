4a038b203c0b613584c18b06edf674aeef7a709e
Miroslav Balev <m.balev@editcreation.com>
2023-12-27 19:06:15 +0200

    table builder import fix

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -4,7 +4,9 @@ export { default as useTable } from './use-table';
 
 export { default as TableNoData } from './table-no-data';
 export { default as TableSkeleton } from './table-skeleton';
+export { default as TableRowCustom } from './table-row-custom';
 export { default as TableEmptyRows } from './table-empty-rows';
 export { default as TableHeadCustom } from './table-head-custom';
 export { default as TableSelectedAction } from './table-selected-action';
 export { default as TablePaginationCustom } from './table-pagination-custom';
+export { default as TableToolbar } from './table-toolbar';
