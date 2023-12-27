9e8800f34a5619476fe6e4ca3ff6d71afbc49b8e
Miroslav Balev <m.balev@editcreation.com>
2023-12-28 00:10:50 +0200

    remove unnecessary console log, format code

--- src/components/hook-form/fields-render.js
+++ src/components/hook-form/fields-render.js
@@ -7,34 +7,34 @@ import RHFTextField from './rhf-text-field';
 import RHFEditor from './rhf-editor';
 
 export default function FieldsRender({ field }) {
-    console.log(field)
-    return (
-        <>
-            {(field.type === 'text' || field.type === 'number' || field.type === 'email' || field.type === 'password') && (
-                <Stack spacing={1.5}>
-                    {field.label && <Typography variant="subtitle2">{field.label}</Typography>}
-                    <RHFTextField
-                        {...field}
-                        name={field.name}
-                        type={field.type}
-                        label={undefined}
-                    />
-                </Stack>
-            )}
 
-            {field.type === 'editor' && (
-                <Stack spacing={1.5}>
-                    {field.label && <Typography variant="subtitle2">{field.label}</Typography>}
-                    <RHFEditor
-                        simple
-                        name={field.name}
-                    />
-                </Stack>
-            )}
-        </>
-    );
+  return (
+    <>
+      {(field.type === 'text' || field.type === 'number' || field.type === 'email' || field.type === 'password') && (
+        <Stack spacing={1.5}>
+          {field.label && <Typography variant="subtitle2">{field.label}</Typography>}
+          <RHFTextField
+            {...field}
+            name={field.name}
+            type={field.type}
+            label={undefined}
+          />
+        </Stack>
+      )}
+
+      {field.type === 'editor' && (
+        <Stack spacing={1.5}>
+          {field.label && <Typography variant="subtitle2">{field.label}</Typography>}
+          <RHFEditor
+            simple
+            name={field.name}
+          />
+        </Stack>
+      )}
+    </>
+  );
 }
 
 FieldsRender.propTypes = {
-    field: PropTypes.object
+  field: PropTypes.object
 }
