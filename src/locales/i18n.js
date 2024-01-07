9814c8e6cc7b67c95513f3cc75a1514c906f228c
Miroslav Balev <m.balev@editcreation.com>
2024-01-07 22:21:51 +0200

    remove commented code and add language detector

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -4,9 +4,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import { localStorageGetItem } from 'src/utils/storage-available';
-
-import { defaultLang } from './config-lang';
 import translationEn from './langs/en.json';
 import translationBg from './langs/bg.json';
 
@@ -15,6 +12,7 @@ import translationBg from './langs/bg.json';
 export default async function initTranslations(locale, i18nInstance) {
   i18nInstance = i18nInstance || i18n.createInstance();
 
+  i18nInstance.use(LanguageDetector);
   i18nInstance.use(initReactI18next);
 
   await i18nInstance.init({
@@ -37,25 +35,3 @@ export default async function initTranslations(locale, i18nInstance) {
     t: i18nInstance.t
   }
 }
-
-// const lng = localStorageGetItem('i18nextLng', defaultLang.value);
-
-// i18n
-//   .use(LanguageDetector)
-//   .use(initReactI18next)
-//   .init({
-//     resources: {
-//       en: { translations: translationEn },
-//       bg: { translations: translationBg }
-//     },
-//     lng,
-//     fallbackLng: lng,
-//     debug: false,
-//     ns: ['translations'],
-//     defaultNS: 'translations',
-//     interpolation: {
-//       escapeValue: false,
-//     },
-//   });
-
-// export default i18n;
