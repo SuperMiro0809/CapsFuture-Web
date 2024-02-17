dc9889c11535ec9fb9575d6aa7d51db33b8b9677
Miroslav Balev <m.balev@editcreation.com>
2024-02-17 18:38:44 +0200

    load from api in home

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -1,4 +1,7 @@
 import { HomeView } from 'src/sections/home/view';
+// api
+import { getUpcomingCampaigns } from 'src/api/campaign';
+import { getLatestProducts } from 'src/api/product';
 
 // ----------------------------------------------------------------------
 
@@ -6,6 +9,30 @@ export const metadata = {
   title: 'Minimal: The starting point for your next project',
 };
 
-export default function HomePage({ params }) {
-  return <HomeView />;
+async function getData(lang) {
+  try {
+    const { data: campaigns, error: campaignsError } = await getUpcomingCampaigns(lang);
+
+    if (campaignsError) throw campaignsError;
+
+    const { data: products, error: productsError } = await getLatestProducts(lang);
+
+    if (productsError) throw productsError;
+
+    return { campaigns, products };
+  } catch (error) {
+    return { error };
+  }
+}
+
+export default async function HomePage({ params }) {
+  const { lang } = params;
+
+  const { campaigns, products, error } = await getData(lang);
+
+  if (error) {
+    return <div>{JSON.stringify(error)}</div>
+  }
+
+  return <HomeView campaigns={campaigns} products={products} />;
 }
