5b4ff3c01c06328a9f9bf61d612de1dbc931d570
Miroslav Balev <m.balev@editcreation.com>
2024-10-06 13:14:17 +0300

    add gestures to map to fix on mobile

--- src/components/leaflet-map/leaflet-map.js
+++ src/components/leaflet-map/leaflet-map.js
@@ -1,12 +1,33 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { useState } from 'react';
+
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { GestureHandling } from 'leaflet-gesture-handling';
+
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-gesture-handling/dist/leaflet-gesture-handling.css';
+
+// ----------------------------------------------------------------------
 
 export default function LeafletMap({ center, zoom, children, ...other }) {
+  const [init, setInit] = useState(true);
+
+  const GestureHandlingSetter = () => {
+    /* eslint-disable */
+    const map = useMap();
+    map.gestureHandling.enable();
+    map.addHandler('gestureHandling', GestureHandling);
+    setInit(false);
+    /* eslint-enable */
+    return null;
+  };
+
   return (
     <MapContainer
       center={center}
       zoom={zoom}
       {...other}
     >
+      {init && <GestureHandlingSetter />}
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
