a398263eea7b3be66552111ae857879f04b777c7
Miroslav Balev <m.balev@editcreation.com>
2024-10-16 18:14:02 +0300

    add new map icon, add location field to campaign form

--- src/components/leaflet-map/icon-list.js
+++ src/components/leaflet-map/icon-list.js
@@ -16,9 +16,17 @@ const stationIcon = new L.Icon({
     popupAnchor: [-3, -76]
 });
 
+const campaignIcon = new L.Icon({
+    iconUrl: '/assets/icons/map/ic_campaign_map_point.svg',
+    iconSize: [64, 64],
+    iconAnchor: [32, 58],
+    popupAnchor: [-3, -76]
+});
+
 // ----------------------------------------------------------------------
 
 export default {
     heartIcon,
-    stationIcon
+    stationIcon,
+    campaignIcon
 };
--- src/sections/campaign/campaign-new-edit-form.js
+++ src/sections/campaign/campaign-new-edit-form.js
@@ -3,13 +3,13 @@ import * as Yup from 'yup';
 import { useMemo, useEffect, useCallback } from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
+import { ASSETS } from 'src/config-global';
 // @mui
 import LoadingButton from '@mui/lab/LoadingButton';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box'
 // hooks
 import { useResponsive } from 'src/hooks/use-responsive';
 // routes
@@ -17,13 +17,15 @@ import { useRouter } from 'src/routes/hooks';
 import { paths } from 'src/routes/paths';
 // api
 import { createCampaign, editCampaign } from 'src/api/campaign';
+import { reverseGeocode } from 'src/api/google-maps';
 // components
 import { useSnackbar } from 'src/components/snackbar';
 import FormProvider, {
-    RHFAutocomplete,
-    RHFDatePicker,
-    RHFLanguageField,
-    RHFUpload
+  RHFDatePicker,
+  RHFLanguageField,
+  RHFUpload,
+  RHFLocationSelectorField,
+  RHFTextField
 } from 'src/components/hook-form';
 // i18
 import { useTranslation } from 'react-i18next';
@@ -31,225 +33,280 @@ import { useTranslation } from 'react-i18next';
 import { format, parseISO } from 'date-fns';
 // utils
 import constructFormData from 'src/utils/form-data';
-// data
-import CITIES from 'src/data/cities';
-import { ASSETS } from 'src/config-global';
 
 export default function CampaignNewEditForm({ currentCampaign }) {
-    const router = useRouter();
+  const router = useRouter();
 
-    const mdUp = useResponsive('up', 'md');
+  const mdUp = useResponsive('up', 'md');
 
-    const { enqueueSnackbar } = useSnackbar();
+  const { enqueueSnackbar } = useSnackbar();
 
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const NewCampaignSchema = Yup.object().shape({
-        title_image: Yup.mixed().required(t('title_image.required', { ns: 'validation' })),
-        date: Yup.date().required(t('date.required', { ns: 'validation' })),
-        cities: Yup.array().min(1, t('cities.required', { ns: 'validation' })),
-        information: Yup.object().shape({
-            bg: Yup.object().shape({
-                title: Yup.string().required(t('title.required', { ns: 'validation' })),
-                short_description: Yup.string().required(t('short_description.required', { ns: 'validation' })),
-                description: Yup.string().required(t('description.required', { ns: 'validation' }))
-            }),
-            en: Yup.object().shape({
-                title: Yup.string().required(t('title.required', { ns: 'validation' })),
-                short_description: Yup.string().required(t('short_description.required', { ns: 'validation' })),
-                description: Yup.string().required(t('description.required', { ns: 'validation' }))
-            })
-        })
-    });
+  const NewCampaignSchema = Yup.object().shape({
+    title_image: Yup.mixed().required(t('title_image.required', { ns: 'validation' })),
+    date: Yup.date().required(t('date.required', { ns: 'validation' })),
+    information: Yup.object().shape({
+      bg: Yup.object().shape({
+        title: Yup.string().required(t('title.required', { ns: 'validation' })),
+        short_description: Yup.string().required(t('short_description.required', { ns: 'validation' })),
+        description: Yup.string().required(t('description.required', { ns: 'validation' }))
+      }),
+      en: Yup.object().shape({
+        title: Yup.string().required(t('title.required', { ns: 'validation' })),
+        short_description: Yup.string().required(t('short_description.required', { ns: 'validation' })),
+        description: Yup.string().required(t('description.required', { ns: 'validation' }))
+      })
+    }),
+    location: Yup.object().required(t('location.required', { ns: 'validation' })),
+    // not required
+    location_note: Yup.string()
+  });
 
-    const defaultValues = useMemo(
-        () => {
-            const citiesValues = currentCampaign?.cities ? currentCampaign.cities.map((el) => el.city) : [];
-
-            const translations = {
-                bg: {
-                    title: '',
-                    short_description: '',
-                    description: ''
-                },
-                en: {
-                    title: '',
-                    short_description: '',
-                    description: ''
-                }
-            };
-
-            if(currentCampaign?.translations) {
-                currentCampaign.translations.forEach((el) => {
-                    translations[el.language] = {
-                        id: el.id,
-                        title: el.title,
-                        short_description: el.short_description,
-                        description: el.description
-                    };
-                })
-            }
-
-            return {
-                title_image: currentCampaign?.title_image_path && { preview: `${ASSETS}/${currentCampaign.title_image_path}` },
-                date: currentCampaign?.date ? parseISO(currentCampaign.date) : null,
-                cities: citiesValues,
-                information: translations
-            }
+  const defaultValues = useMemo(
+    () => {
+      let locationValue = null;
+      const translations = {
+        bg: {
+          title: '',
+          short_description: '',
+          description: ''
         },
-        [currentCampaign]
-    );
-
-    const methods = useForm({
-        resolver: yupResolver(NewCampaignSchema),
-        defaultValues,
-    });
-
-    const {
-        reset,
-        control,
-        watch,
-        setValue,
-        handleSubmit,
-        formState: { isSubmitting },
-    } = methods;
-
-    useEffect(() => {
-        if (currentCampaign) {
-            reset(defaultValues);
+        en: {
+          title: '',
+          short_description: '',
+          description: ''
         }
-    }, [currentCampaign, defaultValues, reset]);
+      };
 
-    const onSubmit = handleSubmit(async (data) => {
-        const { title_image, date } = data;
-        const values = {
-            ...data,
-            date: format(date, 'yyyy-MM-dd'),
-        };
+      if (currentCampaign?.translations) {
+        currentCampaign.translations.forEach((el) => {
+          translations[el.language] = {
+            id: el.id,
+            title: el.title,
+            short_description: el.short_description,
+            description: el.description
+          };
+        });
+      }
 
-        const formData = constructFormData(values);
-
-        if(title_image instanceof File) {
-            formData.append('title_image', title_image);
+      if (currentCampaign?.location) {
+        locationValue = {
+          lat: currentCampaign.location.latitude,
+          lng: currentCampaign.location.longitude
         }
+      }
 
-        try {
-            if(currentCampaign) {
-                await editCampaign(currentCampaign.id, formData);
+      return {
+        title_image: currentCampaign?.title_image_path && { preview: `${ASSETS}/${currentCampaign.title_image_path}` },
+        date: currentCampaign?.date ? parseISO(currentCampaign.date) : null,
+        information: translations,
+        location: locationValue,
+        location_note: currentCampaign.location?.note || ''
+      }
+    },
+    [currentCampaign]
+  );
 
-                enqueueSnackbar(t('edit-success', { ns: 'messages' }));
-            }else {
-                await createCampaign(formData);
+  const methods = useForm({
+    resolver: yupResolver(NewCampaignSchema),
+    defaultValues,
+  });
 
-                enqueueSnackbar(t('create-success', { ns: 'messages' }));
-            }
+  const {
+    reset,
+    control,
+    watch,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
 
-            router.push(paths.dashboard.campaign.root);
-            router.refresh();
-        } catch (error) {
-            enqueueSnackbar(error.message, { variant: 'error' });
+  useEffect(() => {
+    if (currentCampaign) {
+      reset(defaultValues);
+    }
+  }, [currentCampaign, defaultValues, reset]);
+
+  const onSubmit = handleSubmit(async (data) => {
+    const { title_image, date, location, location_note } = data;
+
+    try {
+      const addressBg = await reverseGeocode(location.lat, location.lng, 'bg');
+      const addressEn = await reverseGeocode(location.lat, location.lng, 'en');
+
+      const values = {
+        ...data,
+        date: format(date, 'yyyy-MM-dd'),
+        location: {
+          ...location,
+          address_bg: addressBg,
+          address_en: addressEn,
+          note: location_note
         }
-    });
+      };
 
-    const handleDropSingleFile = useCallback(
-        (acceptedFiles) => {
-            const file = acceptedFiles[0];
+      const formData = constructFormData(values);
 
-            const newFile = Object.assign(file, {
-                preview: URL.createObjectURL(file),
-            });
+      if (title_image instanceof File) {
+        formData.append('title_image', title_image);
+      }
 
-            if (newFile) {
-                setValue('title_image', newFile, { shouldValidate: true });
-            }
-        },
-        [setValue]
-    );
+      if (currentCampaign) {
+        await editCampaign(currentCampaign.id, formData);
 
-    const renderDetails = (
-        <Grid xs={12} md={12}>
-            <Card>
-                <Stack spacing={3} sx={{ p: 3 }}>
-                    <Stack spacing={1.5}>
-                        <Typography variant="subtitle2">{t('title-image', { ns: 'forms' })}</Typography>
-                        <RHFUpload
-                            name="title_image"
-                            maxSize={3145728}
-                            onDrop={handleDropSingleFile}
-                            onDelete={() => setValue('title_image', null, { shouldValidate: true })}
-                        />
-                    </Stack>
+        enqueueSnackbar(t('edit-success', { ns: 'messages' }));
+      } else {
+        await createCampaign(formData);
 
-                    <Box
-                        columnGap={2}
-                        rowGap={3}
-                        display="grid"
-                        gridTemplateColumns={{
-                            xs: 'repeat(1, 1fr)',
-                            md: 'repeat(2, 1fr)',
-                        }}
-                    >
-                        <Stack spacing={1.5}>
-                            <Typography variant="subtitle2">{t('date', { ns: 'forms' })}</Typography>
-                            <RHFDatePicker name='date' />
-                        </Stack>
+        enqueueSnackbar(t('create-success', { ns: 'messages' }));
+      }
 
-                        <Stack spacing={1.5}>
-                            <Typography variant="subtitle2">{t('cities', { ns: 'forms' })}</Typography>
-                            <RHFAutocomplete
-                                name='cities'
-                                options={CITIES}
-                                isOptionEqualToValue={(option, value) => option === value}
-                                multiple
-                            />
-                        </Stack>
-                    </Box>
+      router.push(paths.dashboard.campaign.root);
+      router.refresh();
+    } catch (error) {
+      enqueueSnackbar(error.message, { variant: 'error' });
+    }
+  });
 
-                    <RHFLanguageField
-                        name='information'
-                        langs={[
-                            { label: 'Български', slug: 'bg' },
-                            { label: 'English', slug: 'en' },
-                        ]}
-                        fields={[
-                            { type: 'text', name: 'title', label: t('title', { ns: 'forms' }) },
-                            { type: 'text', name: 'short_description', label: t('short_description', { ns: 'forms' }), multiline: true, rows: 4 },
-                            { type: 'editor', name: 'description', label: t('description', { ns: 'forms' }) }
-                        ]}
-                    />
-                </Stack>
-            </Card>
+  const handleDropSingleFile = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      const newFile = Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      });
+
+      if (newFile) {
+        setValue('title_image', newFile, { shouldValidate: true });
+      }
+    },
+    [setValue]
+  );
+
+  const renderDetails = (
+    <>
+      {mdUp && (
+        <Grid md={4}>
+          <Typography variant="h6" sx={{ mb: 0.5 }}>
+            {t('details', { ns: 'common' })}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {t('details-subtext', { ns: 'common' })}
+          </Typography>
         </Grid>
-    );
+      )}
 
-    const renderActions = (
-        <>
-            {mdUp && <Grid md={4} />}
-            <Grid xs={12} md={8} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'end' }}>
-                <LoadingButton
-                    type="submit"
-                    variant="contained"
-                    size="large"
-                    loading={isSubmitting}
-                    sx={{ ml: 2 }}
-                >
-                    {!currentCampaign ? t('create', { ns: 'common' }) : t('save', { ns: 'common' })}
-                </LoadingButton>
-            </Grid>
-        </>
-    );
+      <Grid xs={12} md={8}>
+        <Card>
+          {!mdUp && <CardHeader title="Details" />}
 
-    return (
-        <FormProvider methods={methods} onSubmit={onSubmit}>
-            <Grid container spacing={3}>
-                {renderDetails}
+          <Stack spacing={3} sx={{ p: 3 }}>
+            <Stack spacing={1.5}>
+              <Typography variant="subtitle2">{t('title-image', { ns: 'forms' })}</Typography>
+              <RHFUpload
+                name="title_image"
+                maxSize={3145728}
+                onDrop={handleDropSingleFile}
+                onDelete={() => setValue('title_image', null, { shouldValidate: true })}
+              />
+            </Stack>
 
-                {renderActions}
-            </Grid>
-        </FormProvider>
-    );
+            <Stack spacing={1.5}>
+              <Typography variant="subtitle2">{t('date', { ns: 'forms' })}</Typography>
+              <RHFDatePicker name='date' />
+            </Stack>
+
+            <RHFLanguageField
+              name='information'
+              langs={[
+                { label: 'Български', slug: 'bg' },
+                { label: 'English', slug: 'en' },
+              ]}
+              fields={[
+                { type: 'text', name: 'title', label: t('title', { ns: 'forms' }) },
+                { type: 'text', name: 'short_description', label: t('short_description', { ns: 'forms' }), multiline: true, rows: 4 },
+                { type: 'editor', name: 'description', label: t('description', { ns: 'forms' }) }
+              ]}
+            />
+          </Stack>
+        </Card>
+      </Grid>
+    </>
+  );
+
+  const renderLocations = (
+    <>
+      {mdUp && (
+        <Grid md={4}>
+          <Typography variant="h6" sx={{ mb: 0.5 }}>
+            {t('location', { ns: 'forms' })}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {t('subtext', { ns: 'location' })}
+          </Typography>
+        </Grid>
+      )}
+
+      <Grid xs={12} md={8}>
+        <Card>
+          {!mdUp && <CardHeader title={t('location', { ns: 'common' })} />}
+
+          <Stack spacing={3} sx={{ p: 3 }}>
+
+            <Stack spacing={1.5}>
+              <RHFLocationSelectorField
+                name='location'
+                placeholder={t('enter-address', { ns: 'location' })}
+                iconName='campaignIcon'
+                helperText={t('enter-address-or-choose-on-map', { ns: 'location' })}
+              />
+            </Stack>
+
+            <Stack spacing={1.5}>
+              <Typography variant="subtitle2">{t('note', { ns: 'forms' })}</Typography>
+              <RHFTextField
+                name='location_note'
+                multiline
+                rows={2}
+              />
+            </Stack>
+          </Stack>
+        </Card>
+      </Grid>
+    </>
+  );
+
+  const renderActions = (
+    <>
+      {mdUp && <Grid md={4} />}
+      <Grid xs={12} md={8} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'end' }}>
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          size="large"
+          loading={isSubmitting}
+          sx={{ ml: 2 }}
+        >
+          {!currentCampaign ? t('create', { ns: 'common' }) : t('save', { ns: 'common' })}
+        </LoadingButton>
+      </Grid>
+    </>
+  );
+
+  return (
+    <FormProvider methods={methods} onSubmit={onSubmit}>
+      <Grid container spacing={3}>
+        {renderDetails}
+
+        {renderLocations}
+
+        {renderActions}
+      </Grid>
+    </FormProvider>
+  );
 }
 
 CampaignNewEditForm.propTypes = {
-    currentCampaign: PropTypes.object,
+  currentCampaign: PropTypes.object,
 };
